da0a8f054406832ab02b0622eb468265fd2e9119
RasenBunkai <sanjixenon553@outlook.com>
2025-02-01 21:08:41 -0500

    Se refactorizo la app para obtener los usuarios mediante un array

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import './App.css'
 import { XFollowCard } from './XFollowCard.jsx'
-
+const users = [
+    {
+        userName: 'Rasen_bunkai',
+        name: 'レイセン枯れ'
+    },
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán'
+    },
+    {
+        userName: 'pucolab',
+        name: 'Pucolab'
+    },
+    {
+        userName: 'MoureDev',
+        name: 'Brais Moure'
+    }
+];
 export function App (){
     return (
         <article className='App'>
-       <XFollowCard
-        userName="Rasen_bunkai"
-        name="レイセン枯れ"
-       />
-       <XFollowCard
-        userName="midudev"
-        name="Miguel Ángel Durán"
-       />
-       <XFollowCard
-        userName="pucolab"
-        name="Pucolab"
-        />
-        <XFollowCard
-        userName="MoureDev"
-        name="Brais Moure"
-        />
+            {
+                users.map((user) => (
+                    <XFollowCard
+                    key={user.userName}
+                    userName={user.userName}
+                    name={user.name}
+                    />
+                ))
+            }
        </article>
     )
 }
